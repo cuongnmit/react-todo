61dad29ad279558e4f9d45a8804eed322bea57b4
Nguyễn Mạnh Cường <nguyenmanhcuong03@vccorp.vn>
2020-05-17 05:23:10 +0700

    Implement todo list

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../containers/Header';
+import MainSection from '../containers/MainSection';
+
+const App = () => (
+  <section className="todoapp">
+    <div>
+      <Header />
+      <MainSection />
+    </div>
+  </section>
+);
+
+export default App;
